f91a68f6ddd21b35759e8b99012014a77b5d5944
sungyean Hong <hosuye@gmail.com>
2025-10-21 16:05:48 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import  Box from './Component/Box'
+import  Box from './component/Box'
 
 const choice={
   rock:{ 
